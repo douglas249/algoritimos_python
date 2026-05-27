00e77cd8c3b840a97f8064fba65ec50fdc086cf0
douglas249 <rodriguescristiandouglas@gmail.com>
2026-05-27 20:44:52 -0300

    commit

---
+++ .venv/ext05.py
@@ -0,0 +1,4 @@
+gg = int(input('qual é o primeir valor? '))
+ii = int(input('qual é o segundo valor?'))
+valor = gg * ii
+print ('o primeiro valor é {} e o segundo valor é {} e a soma total é {}'.format(gg,ii,valor))
