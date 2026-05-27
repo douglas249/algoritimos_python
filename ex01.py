6c1e180bfa222ad12d2004195a9a6ccb9ec3ab9b
douglas249 <rodriguescristiandouglas@gmail.com>
2026-05-27 20:21:17 -0300

    x01

---
+++ ex01.py
@@ -0,0 +1,9 @@
+#o primeiro programa a gente nunca esquece!#/
+
+nome = input('seu nome?')
+cidade = str(input('qual é a sua cidade'))
+municipio = str(input('seu municipio'))
+
+print ('seu nome é {}'.format(nome))
+print ('sua cidade é {}'.format(cidade))
+print ('seu municipio é {}'.format(municipio))
